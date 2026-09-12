ac8f291574964545b6b21e62e67b78f0f97642e0
Somesh Kartikeya <somesh@Someshs-MacBook-Air.local>
2026-09-12 18:46:23 +0530

    Initial commit: Spring Boot backend scaffold with security, JPA, Flyway, Swagger

---
+++ src/main/java/com/mycompass/MycompassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mycompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycompassBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycompassBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompass/MycompassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycompassBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
